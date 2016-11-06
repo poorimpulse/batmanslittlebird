70eec1f3cad8645e4ca2e3ef5028f03aa2eeef73
Russell Hay <me@russellhay.com>
2016-11-05 17:36:38 -0700

    new theme modifications and removing audiojs

--- themes/blb/static/js/app.js
+++ themes/blb/static/js/app.js
@@ -1,18 +1,11 @@
 (function() {
   var currentEpisode = null;
   var audio = null;
-  audiojs.events.ready(function () {
-    audio = audiojs.create(document.getElementsByTagName('audio')[0], {
-
-    });
-  });
 
   function handleClick(element) {
     var id = element.dataset.id;
     showEpisode(id);
-    setHeroUnit(id);
-    // audio.load(element.dataset.src);
-    // audio.play();
+    playEpisode(id);
   }
 
   function unhighlightCurrentEpisode() {
@@ -22,12 +15,28 @@
     }
   }
 
-  function setHeroUnit(id) {
-    var heroElement = document.getElementById('hero-' + id);
-    var index = parseInt(heroElement.dataset.index);
-    var heroViewport = heroElement.parentElement;
-    var offset = (index+1) * 250;
-    heroViewport.style.top = "" + (-offset + 16) + "px";
+  function playEpisode(id) {
+    stopCurrentEpisode();
+    var player = document.getElementById('player');
+    var playButton = document.querySelector('#episode-' + id + ' .play-button');
+    var playButtonText = playButton.querySelector('i');
+    player.src = playButton.dataset['url'];
+    playButtonText.className = 'fa fa-pause-circle fa-fw fa-lg';
+    player.play();
+    player.onended = function() {
+      stopCurrentEpisode();
+      player.src = null;
+    }
+  }
+
+  function stopCurrentEpisode() {
+    var player = document.getElementById('player');
+    player.pause();
+    player.onended = null;
+    var playButtonText = document.querySelector('.fa.fa-pause-circle');
+    if (playButtonText) {
+      playButtonText.className = 'fa fa-play-circle fa-fw fa-lg';
+    }
   }
 
   function showEpisode(id) {
